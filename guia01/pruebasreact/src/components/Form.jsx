349a4c0022abb1987cd54016eccc8a4eea6512e7
sm180297 <edwinsantos74447@gmail.com>
2021-08-21 16:35:29 -0300

    se agrego segundo text

--- guia01/pruebasreact/src/components/Form.jsx
+++ guia01/pruebasreact/src/components/Form.jsx
@@ -2,10 +2,11 @@ import React, {useState} from 'react'
 import Todo from '../components/Todo';
 const Form = () => {
     const [todo, setTodo] = useState({})
+    const [todoo, setTodoo] = useState({})
     const [todos, setTodos] = useState( [
-        {todo: 'todo 1'},
-        {todo: 'todo 2'},
-        {todo: 'todo 3'}
+        {todoo: 'todo ', todo: 'todo 1'},
+        {todoo: 'todo ',todo: 'todo 2'},
+        {todoo: 'todo ', todo: 'todo 3'}
     ])
     const handleChange = e => setTodo({[e.target.name]: e.target.value})
     const handleClick = e => {
@@ -26,6 +27,7 @@ return
         <form onSubmit={e => e.preventDefault()}>
 <label>Agregar tarea</label><br/>
 <input type="text" name="todo" onChange={handleChange}/>
+<input type="text" name="todoo" onChange={handleChange}/>
 <button onClick={handleClick}>agregar</button>
             </form>
        {
